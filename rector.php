15e4e0b1f0d78bdb738c3552cee6217d8cd90f16
MGatner <mgatner@icloud.com>
2022-10-09 14:11:12 +0000

    Fix Rector pipeline version

--- rector.php
+++ rector.php
@@ -83,6 +83,7 @@ return static function (RectorConfig $rectorConfig): void {
         // Ignore files that should not be namespaced
         NormalizeNamespaceByPSR4ComposerAutoloadRector::class => [
             __DIR__ . '/app/Helpers',
+            __DIR__ . '/tests/_support',
         ],
 
         // May load view files directly when detecting classes
